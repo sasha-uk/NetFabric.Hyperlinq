fea4af9030331dcf736a70fca4019900a3e18e40
Antao Almada <antao.almada@gmail.com>
2020-09-16 21:46:00 +0100

    Add unit tests for source generator

---
+++ NetFabric.Hyperlinq.SourceGenerator.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace NetFabric.Hyperlinq.SourceGenerator.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
